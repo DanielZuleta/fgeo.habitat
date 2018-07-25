2cdc08dd3201ab147687af7ef297779dbd3b1c80
maurolepore <maurolepore@gmail.com>
2018-07-25 03:21:12 -0400

    Add interpretation.

--- R/tt_test.R
+++ R/tt_test.R
@@ -32,15 +32,41 @@
 #'   calculated.
 #' @param allabund20 The output of `abund_index()`.
 #'
-#' @author Sabrina Russo, Daniel Zuleta, Matteo Detto, and Kyle Harms.
-#'
 #' @seealso [to_df()].
-#'
+#' 
+#' @author Sabrina Russo, Daniel Zuleta, Matteo Detto, and Kyle Harms.
+#' 
+#' @section Acknowledgments:
+#' Nestor Engone Obiang, David Kenfack, Jennifer Baltzer, and Rutuja
+#' Chitra-Tarak provided feedback. Daniel Zuleta provided guidance.
+#' 
 #' @return
 #' * `tt_test()`: A dataframe.
 #' * `torusonesp.all()`: A numeric matrix.
+#' 
+#' @section Interpretation of Output:
+#' * `N.Hab.1`: Count of stems of the focal species in habitat 1.
+#' * `Gr.Hab.1`: Count of instances the observed relative density of the focal
+#' species on habitat 1 was greater than the relative density based on the TT
+#' habitat map.
+#' * `Ls.Hab.1`: Count of instances the observed relative density of the focal
+#' species on habitat 1 was less than the relative density based on the TT
+#' habitat map.
+#' * `Eq.Hab.1`: Count of instances the observed relative density of the focal
+#' species on habitat 1 was equal to the relative density based on the TT
+#' habitat map.
+#' The sum of the `Gr.Hab.x`, `Ls.Hab.x`, and `Eq.Hab.x` columns for one habitat
+#' equals the number of 20 x20 quads in the plot.
+#' The `Rep.Agg.Neut` columns for each habitat indicate whether the species is
+#' significantly repelled (-1), aggregated (1), or neutrally distributed (0) on
+#' the habitat in question.
+#' The probabilities associated with the test for whether these patterns are
+#' statistically significant are in the `Obs.Quantile` columns for each habitat.
+#' Note that to calculate the probability for repelled, it is the value given,
+#' but to calculate the probability for aggregated, it is 1- the value given.
 #'
 #' @export
+#' 
 #' @examples
 #' # For easier data wranging
 #' library(dplyr)
--- tmp.R
+++ tmp.R
@@ -1,25 +1,6 @@
-Thanks to all that provided feedback:
-  Rutuja
-Nestor
-David
-Jenn
+dfzuleta@gmail.com
+KenfackD@si.edu
+engoneobiangnestor@gmail.com
+arutuj@gmail.com
+jbaltzer@wlu.ca
 
-
-# @section Interpretation of Output:
-
-# `N.Hab.1`: Count of stems of the focal species in habitat 1.
-
-# `Gr.Hab.1`: Count of instances the observed relative density of the focal species on habitat 1 was greater than the relative density based on the TT habitat map.
-
-# `Ls.Hab.1`: Count of instances the observed relative density of the focal species on habitat 1 was less than the relative density based on the TT habitat map.
-
-# `Eq.Hab.1`: Count of instances the observed relative density of the focal species on habitat 1 was equal to the relative density based on the TT habitat map.
-
-
-# The sum of the `Gr.Hab.x`, `Ls.Hab.x`, and `Eq.Hab.x` columns for one habitat equals the number of 20 x20 quads in the plot.
-
-# The `Rep.Agg.Neut` columns for each habitat indicate whether the species is significantly repelled (-1), aggregated (1), or neutrally distributed (0) on the habitat in question.
-
-# The probabilities associated with the test for whether these patterns are statistically significant are in the `Obs.Quantile` columns for each habitat.
-
-# Note that to calculate the probability for repelled, it is the value given, but to calculate the probability for aggregated, it is 1- the value given.
