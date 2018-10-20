1ab605e42cad2fa00b55099ffd462d8bc64c6983
maurolepore <maurolepore@gmail.com>
2018-10-20 15:40:47 -0400

    Use appveyor.

---
+++ tmp.R
@@ -0,0 +1,4 @@
+# Appeveyor badge ---------------------------------------------------------
+
+[![AppVeyor build status](https://ci.appveyor.com/api/projects/status/github/forestgeo/fgeo.habitat?branch=master&svg=true)](https://ci.appveyor.com/project/forestgeo/fgeo.habitat)
+
