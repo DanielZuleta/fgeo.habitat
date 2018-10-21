fa09b32555b2bbadaf24d70c2577c8b4382cd0f3
maurolepore <maurolepore@gmail.com>
2018-10-20 22:08:18 -0400

    Extract funs from ctfs into fgeo.ctfs. Closes #56.

--- R/imports.R
+++ R/imports.R
@@ -1,11 +1,6 @@
 #' @import geoR
 #' @importFrom MASS boxcox
-#' @importFrom stats nls
-#' @importFrom stats rnorm
-#' @importFrom stats var
-#' @importFrom stats nls.control
-#' @importFrom stats predict
-#' @importFrom stats resid
+#' @importFrom stats nls rnorm var nls.control predict resid
 #' @importFrom glue glue
 #' @importFrom rlang abort inform warn expr eval_tidy expr_label %||%
 #' @importFrom fgeo.tool to_df
